a23b1e75bc54264843d813c631c7227e31c431b7
nagcode <nag.app.dev@gmail.com>
2013-07-24 15:17:25 -0700

    Second commit

--- src/com/test/dynamicproxy/Test.java
+++ src/com/test/dynamicproxy/Test.java
@@ -5,6 +5,6 @@ public class Test {
 	public static void main(String args[]) {
 		Service service = ServiceFactory.getService();
 		service.method1("test1", "test2");
-		service.method1("test3", "test4");
+		service.method2("test3", "test4");
 	}
 }
